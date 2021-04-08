b628f033825afcbfc7c11cb6313ddf579af83da7
Ram Nageena Chauhan <ram.nageena88@hotmail.com>
2021-04-08 10:39:17 +0530

    this is first commit

---
+++ CheckMySpelling.js
@@ -0,0 +1,13 @@
+// add a spell check extension to VS Code
+// most can handle errors in all the common text formats,
+// including JavaScript, Typescript, HTML, JSON, C# 
+
+function LiveDemo() {
+  
+  var message;
+  message = "Welcome to Linkedn Academey"
+  
+  var errorMessage;
+  errorMessage = "Cannott complete the action!";
+
+}
